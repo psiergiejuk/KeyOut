34e023b9b83ce2d716e53bfb6e508b85638a0d46
Paweł Siergiejuk <pawelsiergiejuk@gmail.com>
2024-12-02 22:23:47 +0100

    add system font

--- test.py
+++ test.py
@@ -116,10 +116,10 @@ def generate_keyboard_layout(max_x, max_y):
     rows = [
         ["Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Back"],
         ["~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
-        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Shift"],
-        ["Fn", "Ctrl", "Alt", "Space", "Alt", "Ctrl", "Left", "Down", "Up", "Right"]
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter", "Enter"],
+        ["Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Up", "Shift"],
+        ["Fn", "Ctrl", "Alt", "Space", "Space", "Alt", "Ctrl", "Left", "Down", "Right"]
     ]
 
     layout = []
@@ -224,7 +224,7 @@ def main():
                     ecodes.KEY_X, ecodes.KEY_C, ecodes.KEY_V, ecodes.KEY_B, ecodes.KEY_N, ecodes.KEY_M,
                     ecodes.KEY_COMMA, ecodes.KEY_DOT, ecodes.KEY_SLASH, ecodes.KEY_LEFTSHIFT, ecodes.KEY_LEFTCTRL,
                     ecodes.KEY_LEFTALT, ecodes.KEY_SPACE, ecodes.KEY_RIGHTALT, ecodes.KEY_RIGHTCTRL,
-                    ecodes.KEY_UP, ecodes.KEY_DOWN, ecodes.KEY_LEFT, ecodes.KEY_RIGHT
+                    ecodes.KEY_UP, ecodes.KEY_DOWN, ecodes.KEY_LEFT, ecodes.KEY_RIGHT, ecodes.KEY_BACKSPACE,
                 ],
                 ecodes.EV_SYN: [],
                 ecodes.EV_MSC: [ecodes.MSC_SCAN],
@@ -250,6 +250,8 @@ def main():
                 if awaiting_coordinates and x is not None and y is not None:
                     print(f"Processing Touch Down: X={x}, Y={y}")
                     key = map_touch_to_key(x, y, layout)
+                    if key == "Fn":
+                        layout = generate_keyboard_layout(max_x, max_y)                        
                     if key and pressed_key is None:
                         send_key_to_system(ui, key, is_pressed=True)
                         pressed_key = key
